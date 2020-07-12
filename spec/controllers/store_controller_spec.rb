1fadc68e7387436785d2d745c4f548aba3a01058
Kamil Celejewski <kamil12283@gmail.com>
2020-07-12 15:42:47 +0200

    Add store index

---
+++ spec/controllers/store_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe StoreController, type: :controller do
+  describe 'GET #index' do
+    before { get :index }
+
+    describe 'successful response' do
+      it { expect(response).to be_successful }
+      it { expect(response).to render_template :index }
+    end
+
+    context 'product' do
+      let!(:product1) { create :product }
+      let!(:product2) { create :product }
+
+      it 'should return all products' do
+        expect(assigns(:products)).to match_array([product1, product2])
+      end
+    end
+  end
+end
